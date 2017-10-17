a869b57793339b5e3329ec1f8adba59b65602c15
eghensley <eghensley@email.wm.edu>
2017-10-17 11:17:25 -0400

    knn

--- modelresultsandparams.py
+++ modelresultsandparams.py
@@ -12,4 +12,5 @@ Created on Tue Oct 17 09:38:33 2017
 ### SVC(C = 2, kernel = 'sigmoid', gamma = .01), acc: 0.520628683694, roc: 0.520694449743, kappa: 0.0412869219299	, f1: 0.51179955846
 ### SVC(C = 10, kernel = 'poly', gamma = .1, degrees = 3), acc: 0.523903077931, roc: 0.524549324939, kappa: 0.0490053701801, f1: 0.518624185229
 ### SVC(C = .005, kernel = 'linear'), acc:  0.520410, roc: 0.519155, kappa:  0.038282, f1: 0.463038
+### KNeighborsClassifier(n_neighbors = 17, weights = 'distance', algorithm = 'ball_tree', leaf_size = 10) acc: 0.509932329186, roc: 0.510384834906, kappa: 0.0207323945751, f1: 0.515142730746 
 
