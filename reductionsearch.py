c1c60848c61a6aba79b16a5dce9916b3f8a2bc4f
erichensleyibm <eric.hensley@ibm.com>
2017-11-13 11:21:24 -0500

    svc

--- reductionsearch.py
+++ reductionsearch.py
@@ -6,52 +6,58 @@ Created on Thu Nov  9 15:55:19 2017
 @author: eric.hensleyibm.com
 """
 
-from sklearn.manifold import Isomap, SpectralEmbedding, LocallyLinearEmbedding
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA, NMF
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.naive_bayes import GaussianNB 
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, QuantileTransformer, Normalizer
 import pandas as pd
+from sklearn.svm import LinearSVC
+
+#pca but isomap was close 
 
 data = pd.read_csv('train_ml_data.csv')
 y = data['y']
 x_feat = ['totalbasset', 'totalvs_top10', 'shareBDF', 'shareMOR', 'homeawaydiff', 'shareCGV', 'fieldeffect', 'diffsos', 'diffconsistency', 'difffut_sos', 'shareBIL', 'totalDES', 'difflast5', 'shareHOW', 'totalseas_sos', 'shareSAG', 'totalsos', 'shareMAS', 'totalfut_sos', 'diffMAS', 'sharePIG', 'diffSAG', 'shareDOK', 'diffBRN', 'shareDES', 'totalPIG', 'diffluck', 'totalluck', 'totallast5', 'shareARG', 'totalMAR', 'totalSAG', 'shareMAR', 'shareLAZ', 'totalARG', 'totallast10', 'totalCGV', 'totalBIL', 'diffBDF', 'diffseas_sos', 'diffCGV', 'totalpredictive', 'diffDOK', 'diffbasset', 'totalconsistency', 'shareBRN', 'totalBDF', 'sharebasset', 'diffMAR', 'diffvs_top10', 'diffpredictive', 'difflast10', 'totalMAS', 'diffHOW', 'totalBRN', 'totalHOW', 'diffDES', 'diffLAZ', 'diffBIL', 'diffMOR', 'totalMOR', 'diffPIG', 'totalDOK', 'diffARG', 'totalLAZ']
 x = data[x_feat]
-
+#SelectKBest(chi2, k=6).fit_transform(x,y)
+#
+#x = StandardScaler().fit_transform(x)
+#x = MinMaxScaler().fit_transform(x)
 pipe = Pipeline([
-    ('preprocess', StandardScaler()),
-    ('reduce_dim', PCA()),
-    ('classify', GaussianNB())
+    ('preprocess', MinMaxScaler()),
+    ('reduce_dim', PCA(iterated_power=7, random_state = 86)),
+    ('classify', LinearSVC(random_state = 1108))
 ])
 
-N_FEATURES_OPTIONS = range(2, 64)
+N_FEATURES_OPTIONS = range(2, 28)
+C_OPTIONS = np.logspace(-3,3,8)
+#N_FEATURES_OPTIONS = [StandardScaler(), MinMaxScaler(), RobustScaler(), QuantileTransformer(), Normalizer()]
 param_grid = [
     {
-        'reduce_dim': [PCA(iterated_power=7), NMF(random_state = 86), Isomap(), SpectralEmbedding(random_state = 86), LocallyLinearEmbedding(random_state = 86)],
         'reduce_dim__n_components': N_FEATURES_OPTIONS,
-    },
-    {
-        'reduce_dim': [SelectKBest(chi2)],
-        'reduce_dim__k': N_FEATURES_OPTIONS,
+        'preprocess': [StandardScaler(), MinMaxScaler(), RobustScaler(), QuantileTransformer(random_state = 46), Normalizer()],
+        'classify__C': C_OPTIONS,
     },
 ]
-reducer_labels = ['PCA', 'NMF', 'Isomap', 'Spectral', 'LLE', 'KBest(chi2)']
 
-grid = GridSearchCV(pipe, cv=25, n_jobs=-1, param_grid=param_grid, verbose = 3)
+
+reducer_labels = ['StandardScaler', 'MinMaxScaler', 'RobustScaler', 'QuantileTransformer', 'Normalizer']
+
+grid = GridSearchCV(pipe, cv=StratifiedKFold(n_splits = 50, shuffle = True, random_state = 86), n_jobs=-1, param_grid=param_grid, verbose = 4, scoring = ['neg_log_loss', 'accuracy'])
 grid.fit(x, y)
 
+#grid.cv_results_[mean_test_scre_accuracy]
 mean_scores = np.array(grid.cv_results_['mean_test_score'])
 # scores are in the order of param_grid iteration, which is alphabetical
-mean_scores = mean_scores.reshape(1, -1, len(N_FEATURES_OPTIONS))
+mean_scores = mean_scores.reshape(len(C_OPTIONS), -1, len(N_FEATURES_OPTIONS))
 # select score for best C
 mean_scores = mean_scores.max(axis=0)
 bar_offsets = (np.arange(len(N_FEATURES_OPTIONS)) *
                (len(reducer_labels) + 1) + .5)
-
+list(mean_scores[0]).index(max(mean_scores[0]))
+#max(mean_scores[0]) = .7471576446631597
 plt.figure()
 COLORS = 'bgrcmyk'
 for i, (label, reducer_scores) in enumerate(zip(reducer_labels, mean_scores)):
@@ -61,12 +67,5 @@ plt.title("Comparing feature reduction techniques")
 plt.xlabel('Reduced number of features')
 plt.xticks(bar_offsets + len(reducer_labels) / 2, N_FEATURES_OPTIONS)
 plt.ylabel('Digit classification accuracy')
-plt.ylim((0, 1))
-plt.legend(loc='upper left')
-import itertools
-
-x_feat = ['totalbasset', 'totalvs_top10', 'shareBDF', 'shareMOR', 'homeawaydiff', 'shareCGV', 'fieldeffect', 'diffsos', 'diffconsistency', 'difffut_sos', 'shareBIL', 'totalDES', 'difflast5', 'shareHOW', 'totalseas_sos', 'shareSAG', 'totalsos', 'shareMAS', 'totalfut_sos', 'diffMAS', 'sharePIG', 'diffSAG', 'shareDOK', 'diffBRN', 'shareDES', 'totalPIG', 'diffluck', 'totalluck', 'totallast5', 'shareARG', 'totalMAR', 'totalSAG', 'shareMAR', 'shareLAZ', 'totalARG', 'totallast10', 'totalCGV', 'totalBIL', 'diffBDF', 'diffseas_sos', 'diffCGV', 'totalpredictive', 'diffDOK', 'diffbasset', 'totalconsistency', 'shareBRN', 'totalBDF', 'sharebasset', 'diffMAR', 'diffvs_top10', 'diffpredictive', 'difflast10', 'totalMAS', 'diffHOW', 'totalBRN', 'totalHOW', 'diffDES', 'diffLAZ', 'diffBIL', 'diffMOR', 'totalMOR', 'diffPIG', 'totalDOK', 'diffARG', 'totalLAZ']
-
-for L in range(60, len(x_feat)+1):
-  for subset in itertools.permutations(x_feat, L):
-    print(subset)
+plt.ylim((.7, .8))
+plt.legend(loc='upper right')
